ef9e18d5716c2c33764e7df5035c8875e2fc142b
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-06-30 09:22:39 +0530

    just created first version

--- projects/rockPaperScissor/v1/script.js
+++ projects/rockPaperScissor/v1/script.js
@@ -9,58 +9,68 @@ function gameStart(event){
     const target=event.target;
     if(target.classList.contains("btn")){
         let userChoice=target.dataset.choice;
-        compareChoice(userChoice);
+        let resultMessage=compareChoice(userChoice);
+        Ui(resultMessage);
+        console.log(`Current Scores -> User: ${userScore} | Computer: ${computerScore} | Draws: ${draw}`);
+
     }
 };
 function compareChoice(userChoice){
     let computerChoice = computer[Math.floor(Math.random() * computer.length)];
     console.log(`computer choice ${computerChoice}`);
     console.log(`user choice ${userChoice}`);
-    if(computerChoice==="Rock" || userChoice==="Scissor"){
+    if(computerChoice==="Rock" && userChoice==="Scissor"){
         computerScore++;
-        return;
+        return "C";
     }
-    if(computerChoice==="Paper" || userChoice==="Rock"){
+    if(computerChoice==="Paper" && userChoice==="Rock"){
        computerScore++;
-       return; 
+       return "C"; 
     }
-    if(computerChoice==="Scissor" || userChoice==="Paper"){
+    if(computerChoice==="Scissor" && userChoice==="Paper"){
         computerScore++;
-        return;
+        return "C";
     }
-    if(computerChoice==="Rock" || userChoice==="Paper"){
+    if(computerChoice==="Rock" && userChoice==="Paper"){
         userScore++;
-        return;
+        return "U";
     }
-    if(computerChoice==="Paper" || userChoice==="Scissor"){
+    if(computerChoice==="Paper" && userChoice==="Scissor"){
        userScore++;
-       return; 
+       return "U"; 
     }
-    if(computerChoice==="Scissor" || userChoice==="Rock"){
+    if(computerChoice==="Scissor" && userChoice==="Rock"){
         userScore++;
-        return;
+        return "U";
     }
-    if(computerChoice==="Rock" || userChoice==="Rock"){
+    if(computerChoice==="Rock" && userChoice==="Rock"){
        draw++;
-        return;
+        return "D";
     }
-    if(computerChoice==="Paper" || userChoice==="Paper"){
+    if(computerChoice==="Paper" && userChoice==="Paper"){
        draw++;
-        
-       return; 
+       return "D"; 
     }
-    if(computerChoice==="Scissor" || userChoice==="Scissor"){
+    if(computerChoice==="Scissor" && userChoice==="Scissor"){
         draw++;  
-        return;
+        return "D";
     }
-    console.log(`computer score${computerScore}`);
-    console.log(`user score${userScore}`);
-    console.log(`draw${draw}`);
 }
 
-// function Ui(){
-    
-// }
+ function Ui(winner){
+    if(winner==="C"){
+        console.log("computer won");
+        return;
+    }
+    if(winner==="U"){
+        console.log("User won");
+         return;
+    }
+    if(winner==="D"){
+        console.log("Draw ");
+         return;
+    }
+}
 
 
 
