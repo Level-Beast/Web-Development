3980206bb1d3820674c4e3a5e689e33955cc3afb
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-07-30 14:34:53 +0530

    tryna learn array

---
+++ smallprograms/array/script.js
@@ -0,0 +1,42 @@
+// const drinks = ["Water", "Tea", "Coffee"];
+// drinks[1]="Juice"
+// drinks.push("Milk");
+// drinks.shift();
+// console.log(drinks)
+// const marks = [65, 72, 81, 90];
+// marks[1]=75;
+// marks.unshift(100);
+// marks.pop();
+// console.log(marks);
+
+// const cities = ["Delhi", "Mumbai"];
+// cities[2]="Pune";
+// console.log(cities);
+// const tasks = [
+//     "Learn Arrays",
+//     "Practice",
+//     "Watch TV"
+// ];
+// tasks.unshift("wake up ");
+// tasks.pop();
+// tasks.push("Exercise");
+// console.log(tasks);
+
+ //first thing  i will look for the index which i have to replace once removed i will add new to it 
+ const bag = [];
+ bag.push("Notebook");
+ console.log(bag);
+ bag.push("Pen");
+ console.log(bag);
+ bag.push("Laptop");
+ console.log(bag);
+ bag.push("Mouse");
+ console.log(bag);
+
+//challenge 2
+bag[1]="Keyboard";
+bag.push("Headphone");
+
+//challenge 3
+//it will look like this 
+// an array with 4 index first three empty and 4th one is with 100 and its lenght is 1 cuz lenght see all the elements but ikd if it also count empty lol if empty then 4 but this question is kinda tuff lol 
