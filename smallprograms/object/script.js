460c737e1a49190de641325738fa6fe0782e5909
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-08-03 14:56:04 +0530

    just build some stuff

---
+++ smallprograms/object/script.js
@@ -0,0 +1,71 @@
+// // const player={
+// //     Name: "Warrior",
+// //     Health: 100,
+// //     Gold: 50,
+// //     Level: 1,
+// //     Attack: 20
+// // }
+// // player.Gold+=100;
+// // player.Attack+=5;
+// // player.Health-=200;
+// // if(player.Health<=0){
+// //     player.Health=0;
+// // }
+// // if(player.Gold>=120){
+// //     player.Gold-=120;
+// //     console.log(`sword purchased  and gold left ${player.Gold}`);
+// // }
+// // // console.log(player.Name);
+// // // console.log(player.Health);
+// // // console.log(player.Gold);
+// // console.log(player)
+
+// const students = [
+//     {
+//         name: "Alex",
+//         marks: 91
+//     },
+//     {
+//         name: "Raj",
+//         marks: 72
+//     },
+//     {
+//         name: "Sara",
+//         marks: 88
+//     },
+//     {
+//         name: "John",
+//         marks: 65
+//     },
+//     {
+//         name: "Emma",
+//         marks: 99
+//     }
+// ];
+// let hightestMarks=students[0].marks;
+//  let highScorerCount = 0; 
+//  for(let i=0;i<students.length;i++){
+//      // console.log(students[i].name);
+//      // console.log(students[i].marks);
+//      if(students[i].marks>80){
+//            highScorerCount++; 
+//          console.log(`student who scored more then 80 are ${students[i].name}`)
+//      }
+//      if(hightestMarks<students[i].marks){
+//          hightestMarks=students[i].marks;
+//      }
+//      students[i].marks+=5;
+//     if(students[i].marks>100){
+//         students[i].marks=100;
+//     }
+//  }
+//  console.log(highScorerCount);
+//  console.log(hightestMarks);
+
+const students = [
+    { name: "Alex", marks: 91 },
+    { name: "Raj", marks: 72 },
+    { name: "Sara", marks: 88 },
+    { name: "John", marks: 65 },
+    { name: "Emma", marks: 99 }
+];
