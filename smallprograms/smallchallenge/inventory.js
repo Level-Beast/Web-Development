cca473ad4a19d076dcfd62e739c7c8b76cd8e986
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-08-20 08:31:15 +0530

    just trying to build some stuff

---
+++ smallprograms/smallchallenge/inventory.js
@@ -0,0 +1,32 @@
+const prompt = require('prompt-sync')();
+
+const backpack = {
+  ironHelm: 1,
+  woodenShield: 1,
+  healingHerb: 5
+};
+
+const equipment = {
+  head: null,      // Nothing equipped on head yet
+  leftHand: null,  // Nothing equipped in left hand yet
+  rightHand: "rustySword"
+};
+
+const itemTypes = {
+  ironHelm: "head",
+  woodenShield: "leftHand",
+  healingHerb: "consumable" // Consumables CANNOT be equipped!
+};
+function validateItem(item){
+    let itemName=itemTypes[item] ? itemTypes[item]:"this item doesn't exist";
+    let canBeEquipped=itemTypes.healingHerb === itemName ? "this can't be equiped" : "this can be equiped";  
+    console.log(canBeEquipped);
+    return itemName;
+}
+
+function equipItem(itemName){
+    let result=validateItem(itemName);
+    
+}
+let itemName="healingHerb";
+equipItem(itemName);
