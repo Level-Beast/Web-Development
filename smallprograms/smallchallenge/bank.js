cd696d1c70a09a7085cadf35b197cc57ace751a8
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-09-01 08:00:06 +0530

    just trying to improve

--- smallprograms/smallchallenge/bank.js
+++ smallprograms/smallchallenge/bank.js
@@ -1,32 +1,42 @@
 const prompt = require('prompt-sync')();
 
-const inventory={
-    products:{
-        1:{name:"kurkure",price:15},
-        2:{name:"Biscuite",price:25},
-    },
-    isActive:true
-};
 
-function takeOrder(){
-    console.log(`here is our inventory sir ${inventory.products}`)
-    let userResponse=Number(prompt("plz select ur order and enter a number press zero to exist"));
+const gameData={
+    playerHealth:100,
+    EnemyHealth:100,
+    isPlaying:true
+};
+function userInput(){
+    let userResponse=Number(prompt("select the valid option based 1 for attack 2 for heal 3 to exist"));
     return userResponse;
 }
-function validate(response){
-    if(response===0){
-        inventory.isActive=false;
-        return 'user is logged off';
-    }
-    let processed=inventory.products[response];
-    return processed;
-}
-
-function boss (){
-    while(inventory.isActive){
-        let userselected=takeOrder();
-        let process=validate(userselected);
-        console.log(process)
+function attack(){
+    gameData.EnemyHealth-=20;
+    console.log("enemy got damage");
+    console.log(gameData.EnemyHealth);
+    if(gameData.EnemyHealth<=0){
+        console.log( "enemy is dead u won the game");
+        gameData.isPlaying=false;
     }
 }
-boss();
+function heal(){
+    gameData.playerHealth+=10;
+    console.log("u are healing");
+    if(gameData.playerHealth>100){
+        gameData.playerHealth=100
+        console.log(gameData.playerHealth)
+    }
+}
+function gameStart(){
+    while(gameData.isPlaying){
+        let userResponse=userInput();
+        if(userResponse===3){
+            gameData.isPlaying=false;
+            return "u exist the game"
+        }
+        if(userResponse===1)attack();
+        if(userResponse===2)heal();
+    }
+}
+let finalresult=gameStart();
+console.log(finalresult);
