47ab1eaebd6514fa95b5d7addf3f5248d503614a
Jugraj Singh <jugrajsinghbajwa857@gmail.com>
2026-09-04 09:13:09 +0530

    tryna improve

---
+++ smallprograms/object/ticket.js
@@ -0,0 +1,48 @@
+const prompt = require('prompt-sync')();
+
+const tickets={
+    1:{ticketName:"regularTicket", price:150,},
+    2:{ticketName:"premiumTicket",price:300},
+    3:{ticketName:"vipTicket",price:500},
+}
+
+const userTotal={
+    total:0,
+    tickets:[],
+    isBuying:true
+}
+
+function userInput(){
+    let userChoice=Number(prompt("purchase the ticket u want here are options  1 regular price 150  option 2 premium price 300  and option 3 for vip ticket with price 500 for checkout click 4"));
+    return userChoice;
+};
+function proccessingUserChoice(choice){
+    if(choice===4){
+        userTotal.isBuying=false;
+        return;
+    }
+    let ticketData=tickets[choice];
+    addIteam(ticketData);
+}
+
+function addIteam(userData){
+    userTotal.tickets.push(userData);
+    userTotal.total+=userData.price;
+    console.log(`ur iteam is added ${userData.ticketName} successfully`);
+}
+function summery(){
+    console.log(`ur total is ${userTotal.total}`);
+    for(let i=0;i<userTotal.tickets.length;i++){
+        console.log(userTotal[i].ticketName);
+    }
+    console.log(`and u have bought all these tickets ${userTotal.tickets}`);
+}
+function main(){
+    while(userTotal.isBuying){
+        let userChoice=userInput();
+        proccessingUserChoice(userChoice);
+    };
+    summery();
+}
+
+main();
